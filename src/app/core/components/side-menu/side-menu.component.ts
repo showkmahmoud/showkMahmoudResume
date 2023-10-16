0e58032d10a42fe3d5d820868dc0ae80b58c1202
showk mahmoud <showkmahmoud@gmail.com>
2023-10-16 21:45:12 +0300

    BF: cv link

--- src/app/core/components/side-menu/side-menu.component.ts
+++ src/app/core/components/side-menu/side-menu.component.ts
@@ -19,7 +19,7 @@ export class SideMenuComponent implements OnInit {
     link.setAttribute('target', '_blank');
     link.setAttribute(
       'href',
-      'https://drive.google.com/file/d/1Tg7sYd42Bcoh8t0jcb2BVxL8ro1OJAma/view?usp=sharing'
+      'https://drive.google.com/file/d/1xGlQ0qyMSssHVgmyl2n8UOlIAjaCTceR/view?usp=sharing'
     );
     link.setAttribute('download', `CV.pdf`);
     document.body.appendChild(link);
